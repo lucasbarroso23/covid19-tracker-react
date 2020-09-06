40ce8ff43f62c910b163eae8b359c2ea563117c2
Lucas Barroso <lucasbarroso23@gmail.com>
2020-09-06 19:47:22 -0300

    terminando as tabelas e chamadas na api

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -5,7 +5,7 @@ import { Line, Bar } from 'react-chartjs-2';
 import sytles from './Chart.module.css';
 
 
-const Chart = () => {
+const Chart = ({ data: { confirmed, deaths, recovered }, country }) => {
 
     const [dailyData, setDailyData] = useState([]);
 
@@ -38,9 +38,33 @@ const Chart = () => {
         ) : null
     );
 
+    const barChart = (
+        confirmed
+            ? (
+                <Bar
+                    data={{
+                        labels: ['Infected', 'Recovered', 'Deaths'],
+                        datasets: [{
+                            label: 'People',
+                            backgroundColor: [
+                                'rgb(0, 0, 255, 0.5)',
+                                'rgb(0, 255, 0, 0.5)',
+                                'rgb(255, 0, 0, 0.5)',
+                            ],
+                            data: [confirmed.value, deaths.value, deaths.value]
+                        }]
+                    }}
+                    options={{
+                        legend: { display: false },
+                        title: { display: true, text: `Current state in ${country}` },
+                    }}
+                />
+            ) : null
+    );
+
     return (
         <div className={sytles.container}>
-            {lineChart}
+            {country ? barChart : lineChart}
         </div>
     )
 }
